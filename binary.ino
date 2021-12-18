e733ac58baf8fa8158da1d6f8b74d2ed7689d9e9
hawlan <83552463+hawlanjs@users.noreply.github.com>
2021-12-18 22:01:17 +0100

    Create binary.ino

---
+++ binary.ino
@@ -0,0 +1,59 @@
+
+int pin2=2;
+int pin3=3;
+int pin4=4;
+int pint5=5;
+
+void setup() {
+
+  pinMode (pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+   pinMode(pin4,OUTPUT);
+
+   
+}
+
+void loop() {
+
+digitalWrite(pin2,LOW);
+digitalWrite(pin3,LOW);
+digitalWrite(pin4,LOW);
+
+delay(750);
+  
+digitalWrite(pin2,HIGH);
+digitalWrite(pin3,HIGH);
+digitalWrite(pin4,HIGH);
+
+
+
+
+digitalWrite(pin2,LOW);
+digitalWrite(pin3,LOW);
+digitalWrite(pin4,HIGH);
+
+delay(750);
+
+digitalWrite(pin2,LOW);
+digitalWrite(pin3,HIGH);
+digitalWrite(pin4,LOW);
+
+delay(750);
+
+digitalWrite(pin2,LOW);
+digitalWrite(pin3,HIGH);
+digitalWrite(pin4,HIGH);
+
+delay(750);
+
+digitalWrite(pin2,LOW);
+digitalWrite(pin3,HIGH);
+digitalWrite(pin4,HIGH);
+
+delay(750);
+
+
+
+
+
+}
