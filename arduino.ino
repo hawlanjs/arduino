53e73e624984f5d2233f9898cfdee9a259f70528
hawlan <83552463+hawlanjs@users.noreply.github.com>
2022-01-02 15:56:09 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -25,7 +25,7 @@ void loop() {
   Serial.print(distance); // Sends the distance value into the Serial Port
   Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
-  // Repeats the previous lines from 165 to 15 degrees
+ 
   for(int i=165;i>15;i--){  
   myServo.write(i);
   delay(30);
