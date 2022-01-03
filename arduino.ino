0a0e1d46625ba7e1d1bd17d7e434c740a0a58229
hawlan <83552463+hawlanjs@users.noreply.github.com>
2022-01-03 03:50:11 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -9,14 +9,12 @@ void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
-  myServo.attach(12); // Defines on which pin is the servo motor attached
-}
+  myServo.attach(12); 
 void loop() {
-  // rotates the servo motor from 15 to 165 degrees
   for(int i=15;i<=165;i++){  
   myServo.write(i);
   delay(30);
-  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  distance = calculateDistance();
   
   Serial.print(i); 
   Serial.print(",");
